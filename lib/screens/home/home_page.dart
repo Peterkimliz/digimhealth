0ec83090e12fc21043a6a49f40679e04d3db6f66
PeterLojiKim <pkironji@yahoo.com>
2023-03-30 04:51:29 +0300

    added minor title widget

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:digimhealth/utils/styles.dart';
+import 'package:digimhealth/widgets/major_title.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,10 +8,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            children: [
+              MajorTitle(title: "Upcoming Appointments", color: Colors.black),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 150,
+                child: ListView.builder(
+                    itemCount: 10,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: Styles.mainColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              "assets/images/doctor4.jpg"))),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    MajorTitle(
+                                      title: "Dr .Tonia",
+                                      color: Colors.white,
+                                      size: 16,
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/minor_title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MinorTitle extends StatelessWidget {
+  final String title;
+  double? size=14.0;
+  final Color color;
+  final FontWeight ?fontWeight;
+  final int ? maxline=1;
+
+  MinorTitle({Key? key, required this.title, required this.color,this.size,this.fontWeight,this.maxline}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(title,
+      maxLines: maxline,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: fontWeight,
+        overflow: TextOverflow.ellipsis,
+
+      ),
+    );
+  }
+}
