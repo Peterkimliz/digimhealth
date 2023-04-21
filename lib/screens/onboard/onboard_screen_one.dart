1f4a5bb019054488652b39925b9201c659894c74
PeterLojiKim <pkironji@yahoo.com>
2023-04-21 18:17:34 +0300

    finished the appointment details page

--- lib/screens/onboard/onboard_screen_one.dart
+++ lib/screens/onboard/onboard_screen_one.dart
@@ -2,6 +2,26 @@ import 'package:digimhealth/screens/onboard/onboard_widget.dart';
 import 'package:digimhealth/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+
+// SizedBox(
+// width: 200.0,
+// height: 100.0,
+// child: Shimmer.fromColors(
+// baseColor: Colors.red,
+// highlightColor: Colors.yellow,
+// child: Text(
+// 'Shimmer',
+// textAlign: TextAlign.center,
+// style: TextStyle(
+// fontSize: 40.0,
+// fontWeight:
+// FontWeight.bold,
+// ),
+// ),
+// ),
+// );
+
+
 class OnboardScreenOne extends StatelessWidget {
   OnboardScreenOne({Key? key}) : super(key: key);
 
